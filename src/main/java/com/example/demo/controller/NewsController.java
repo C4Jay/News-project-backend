ab9497c395e81274877b8f1be8202252a69f1dc8
C-Jay <choolakejay@gmail.com>
2020-11-08 17:32:47 +0530

    First Controller

--- src/main/java/com/example/demo/controller/NewsController.java
+++ src/main/java/com/example/demo/controller/NewsController.java
@@ -1,4 +1,28 @@
 package com.example.demo.controller;
 
+import com.example.demo.dao.NewsDataAccessService;
+import com.example.demo.models.News;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("api/v1/")
+@RestController
+@CrossOrigin(origins = "*")
 public class NewsController {
+
+    @Autowired
+    private NewsDataAccessService newsDataAccessService;
+
+//    all news
+    @GetMapping("news")
+    public List<News> getAllNews() {
+        return newsDataAccessService.findAll();
+    }
+
+
 }
