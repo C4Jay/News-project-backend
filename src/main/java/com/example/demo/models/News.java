63b4ff0e0c661e4bb7aaa9b862428e276b54e1a0
C-Jay <choolakejay@gmail.com>
2020-11-08 17:27:19 +0530

    skeleton

---
+++ src/main/java/com/example/demo/models/News.java
@@ -0,0 +1,48 @@
+package com.example.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "")
+public class News {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String title;
+    private String body;
+
+    public News(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public News() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
