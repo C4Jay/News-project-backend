e2ab964b94bab10063813836fb876e47071ce3d9
C-Jay <choolakejay@gmail.com>
2020-11-16 19:07:05 +0530

    summerized all

--- src/main/java/com/example/demo/models/CategoricalNews.java
+++ src/main/java/com/example/demo/models/CategoricalNews.java
@@ -8,6 +8,10 @@ public class CategoricalNews {
     private String title;
     private String body;
 //    private Integer cat_id;
+    private String subcategory01;
+    private String subcategory02;
+    private String summerized_article;
+
 
 
 
@@ -17,11 +21,17 @@ public class CategoricalNews {
 
     public CategoricalNews(Integer news_id,
 //                           Integer cat_id,
-                           String title, String body) {
+                           String title, String body,
+                           String subcategory01,
+                           String subcategory02,
+                           String summerized_article) {
         this.news_id = news_id;
 //        this.cat_id = cat_id;
         this.title = title;
         this.body = body;
+        this.subcategory01 = subcategory01;
+        this.subcategory02 = subcategory02;
+        this.summerized_article = summerized_article;
     }
 
     public Integer getNews_id() {
@@ -56,4 +66,27 @@ public class CategoricalNews {
         this.body = body;
     }
 
+    public String getSubcategory01() {
+        return subcategory01;
+    }
+
+    public void setSubcategory01(String subcategory01) {
+        this.subcategory01 = subcategory01;
+    }
+
+    public String getSubcategory02() {
+        return subcategory02;
+    }
+
+    public void setSubcategory02(String subcategory02) {
+        this.subcategory02 = subcategory02;
+    }
+
+    public String getSummerized_article() {
+        return summerized_article;
+    }
+
+    public void setSummerized_article(String summerized_article) {
+        this.summerized_article = summerized_article;
+    }
 }
